a49c3f7de604e86cd185c44723a586f14ed25e0c
piontec <unknown>
2016-10-12 21:46:53 +0200

    logging config

--- docker-enforcer.py
+++ docker-enforcer.py
@@ -1,8 +1,12 @@
+import logging
 import sched
 import signal
 import threading
 import time
+from logging import StreamHandler
 from threading import Thread
+
+import sys
 from flask import Flask
 
 keep_running = True
@@ -12,36 +16,44 @@ counter = 0
 
 def create_app():
     flask_app = Flask(__name__)
-    # thread = None
-    s = sched.scheduler(time.time, time.sleep)
+    if not flask_app.debug:
+        handler = StreamHandler(stream=sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        flask_app.logger.addHandler(handler)
+        flask_app.logger.setLevel(logging.DEBUG)
+
+    scheduler = sched.scheduler(time.time, time.sleep)
 
     def on_exit(sig, frame):
-        print("stopping")
+        flask_app.logger.info("Stopping docker monitoring")
         global keep_running
         keep_running = False
         thread.join()
-        print("Threads complete, ready to finish")
+        flask_app.logger.debug("Threads complete, ready to finish")
         raise KeyboardInterrupt()
 
     def start_something():
-        print("stuff start")
-        s.enter(1, 1, do_something)
-        s.run()
+        flask_app.logger.debug("Starting background thread")
+        scheduler.enter(1, 1, do_something)
+        scheduler.run()
 
     def do_something():
         if not keep_running:
             return
-        print("Doing stuff...")
+        flask_app.logger.debug("Working...")
         global counter
         with padlock:
             counter += 1
         # do your stuff
-        s.enter(1, 1, do_something)
+        scheduler.enter(1, 1, do_something)
 
     thread = Thread(target=start_something)
     thread.start()
     signal.signal(signal.SIGINT, on_exit)
     signal.signal(signal.SIGTERM, on_exit)
+
     return flask_app
 
 app = create_app()
