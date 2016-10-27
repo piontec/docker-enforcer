2fcd61b410b8a3982c843628b6a282cd738c293b
piontec <unknown>
2016-10-27 21:43:58 +0200

    initial unit tests

--- test/test_killer.py
+++ test/test_killer.py
@@ -5,10 +5,9 @@ from test.test_helpers import RulesTestHelper
 class RulesTests(unittest.TestCase):
 
     def test_count_rule(self):
-        rules = [
-            {
-                "name": "no more than 10",
-                "rule": lambda c: c.position > 10
-            }
-        ]
+        rules = [{"name": "no more than 10", "rule": lambda c: c.position > 10}]
         self.assertFalse(RulesTestHelper(rules).get_verdict().verdict)
+
+    def test_no_containers(self):
+        rules = [{"name": "no containers", "rule": lambda c: True}]
+        self.assertTrue(RulesTestHelper(rules).get_verdict().verdict)
